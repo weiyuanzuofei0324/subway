6f42d77a313e3a3cca726fac010d45bfe215db7a
未远昨非 <weiyuanzuofei@gmail.com>
2026-07-18 12:20:45 +0800

    fix:docker

--- server/internal/database/database.go
+++ server/internal/database/database.go
@@ -1,10 +1,31 @@
 package database
 
 import (
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Open(dsn string) (*gorm.DB, error) {
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var lastErr error
+
+	for i := 0; i < 30; i++ {
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			sqlDB, pingErr := db.DB()
+			if pingErr == nil {
+				if pingErr = sqlDB.Ping(); pingErr == nil {
+					return db, nil
+				}
+			}
+			lastErr = pingErr
+		} else {
+			lastErr = err
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+
+	return nil, lastErr
 }
